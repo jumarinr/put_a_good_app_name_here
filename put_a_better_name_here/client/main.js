f429287dd04f0ed141f3c36820725d6c80f99a08
Juan Diego <jumarinr@unal.edu.co>
2018-10-03 02:12:20 -0500

    vue

---
+++ put_a_better_name_here/client/main.js
@@ -0,0 +1,13 @@
+import { Meteor } from "meteor/meteor";
+import Vue from "vue";
+import VueTracker from 'vue-meteor-tracker';
+
+Vue.use(VueTracker);
+// importamos fichero principal
+import App from '../imports/ui/App.vue';
+
+Meteor.startup(() => {
+  new Vue({
+    render: h => h(App),
+  }).$mount('app');
+});
---
+++ put_a_better_name_here/imports/ui/App.vue
@@ -0,0 +1,9 @@
+<template>
+  <h1> Hola meteor desde Vue!</h1>
+</template>
+
+<script>
+</script>
+
+<style>
+</style>
