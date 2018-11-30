b56c23948565361eb1a3e053e1c113d0ccdfe42b
jumarinr <31374735+jumarinr@users.noreply.github.com>
2018-11-29 23:30:05 -0500

    Comentarios de metodos

--- TodoApp/imports/api/tasks.js
+++ TodoApp/imports/api/tasks.js
@@ -3,11 +3,11 @@ import { Mongo } from "meteor/mongo";
 import { check } from "meteor/check";
 
 export const Tasks = new Mongo.Collection("tasks");
-
+//comprobamos que estemos en el servidor
 if (Meteor.isServer) {
-  // This code only runs on the server
-  Meteor.publish("tasks", function tasksPublication() {
-    return Tasks.find();
+  // Esto solo corre en el servidor, por eso lo exportamos alli.
+  Meteor.publish("tasks", function tasksPublication() { //publica en el cliente la tarea
+    return Tasks.find(); 
   });
 }
 
@@ -15,11 +15,11 @@ Meteor.methods({
   "tasks.insert"(text) {
     check(text, String);
 
-    // Make sure the user is logged in before inserting a task
+    // Se asegura que el usuario esta logueado para poder ingresar una nota
     if (!this.userId) {
       throw new Meteor.Error("not-authorized");
     }
-
+    //estandar para guardar la tarea en la collecion tasks
     Tasks.insert({
       text,
       createdAt: new Date(),
@@ -27,28 +27,32 @@ Meteor.methods({
       username: Meteor.users.findOne(this.userId).username
     });
   },
+  //Estandar para borrar la tarea
   "tasks.remove"(taskId) {
     check(taskId, String);
 
     Tasks.remove(taskId);
   },
+  //Verifica si una tarea esta chekeada
   "tasks.setChecked"(taskId, setChecked) {
     check(taskId, String);
     check(setChecked, Boolean);
 
+    //hace un update al estado de las tareas
     Tasks.update(taskId, { $set: { checked: setChecked } });
   },
+  //se encarga de volver privadas o no las tareas
   "tasks.setPrivate"(taskId, setToPrivate) {
     check(taskId, String);
     check(setToPrivate, Boolean);
 
     const task = Tasks.findOne(taskId);
 
-    // Make sure only the task owner can make a task private
+    // Se asegura que solo el usuario que escribio la tarea puede cambiar si es publica o privada
     if (task.owner !== this.userId) {
       throw new Meteor.Error("not-authorized");
-    }
-
+    } 
+    //Se actualiza tasks
     Tasks.update(taskId, { $set: { private: setToPrivate } });
   }
 });
