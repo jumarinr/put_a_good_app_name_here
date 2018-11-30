7eb7a3defe6fcffb08f8e0157da658f753418704
jumarinr <31374735+jumarinr@users.noreply.github.com>
2018-11-29 23:30:57 -0500

    Update accounts-config.js
    Comentario nuevo

--- TodoApp/imports/startup/accounts-config.js
+++ TodoApp/imports/startup/accounts-config.js
@@ -1,4 +1,4 @@
-import { Accounts } from "meteor/accounts-base";
+import { Accounts } from "meteor/accounts-base"; //Importamos para el manejo de cuentas, gracias al paquete de meteor
 
 Accounts.ui.config({
   passwordSignupFields: "USERNAME_ONLY"
